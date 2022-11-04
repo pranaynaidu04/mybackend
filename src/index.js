ef4e9d79f205072d373cd95558623cb233e11fba
pranaynaidu04 <pranaynaidu482@gmail.com>
2022-11-04 13:35:11 +0530

    done with MW-1 assignment

--- src/index.js
+++ src/index.js
@@ -14,6 +14,25 @@ mongoose.connect("mongodb+srv://pranaynaidu04:pranay482@pranayproject.2jetgiq.mo
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
+
+const assignmentMW= function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+ 
+    let ip= req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+}
+
+app.use( assignmentMW )
+
 app.use('/', route);
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,30 +3,65 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const commonMW = require ("../middlewares/commonMiddlewares")
+
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-// router.post("/createUser", UserController.createUser  )
 
-// router.get("/getUsersData", UserController.getUsersData)
+
 
 router.post("/createBook", BookController.createBook  )
 
-// router.get("/createBook1", BookController.createBook1  )
-
-router.get("/bookList", BookController.bookList)
 
 
-router.post("/getBooksInYear", BookController.getBooksInYear  )
 
-router.get("/getParticularBooks", BookController.getParticularBooks)
+// router.post("/createUser", UserController.createUser  )
+// router.get("/getUsersData", UserController.getUsersData)
 
 
-router.get("/getXINRBooks", BookController.getXINRBooks  )
+// const mid1= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid1")
+//     // logic
+//     let loggedIn = false
 
-router.get("/getRandomBooks", BookController.getRandomBooks)
- 
+//     if (loggedIn== true) { 
+//         console.log( "OK LOGGED IS IS TRUE NOW")
+//         next ()
+//     }
+//     else {
+//         res.send ("Please login or register")
+//     }
+// }
+
+// // e.g. restricted and open-to-all API's can be handled like below now:
+// router.get('/homePage', mid1, UserController.feeds)
+// router.get('/profileDetails', mid1, UserController.profileDetails)
+// router.get('/friendList', mid1, UserController.friendList)
+// router.get('/changePassword', mid1, UserController.changePassword)
+
+// router.get('/termsAndConditions',  UserController.termsAndConditions)
+// router.get('/register',  UserController.register)
+
+
+
+
+
+router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+
+//-----------------------------------------------assignmnet mw1---------------------------------------------
+
+
+//router.get("/globalMw", commonMW.assignmentMW)  
+
+// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
+// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
+// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
+
+
+
 
 module.exports = router;
